b93e3ac64318de384cd5adc517f0f4f4154f8db7
Knut Ahlers <knut@ahlers.me>
2021-09-29 13:10:47 +0200

    Remove old dep management / vendoring
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- vendor/github.com/Luzifer/korvike/functions/func_base64.go
+++
@@ -1,9 +0,0 @@
-package functions
-
-import "encoding/base64"
-
-func init() {
-	registerFunction("b64encode", func(name string, v ...string) string {
-		return base64.StdEncoding.EncodeToString([]byte(name))
-	})
-}
--- vendor/github.com/Luzifer/korvike/functions/func_now.go
+++
@@ -1,9 +0,0 @@
-package functions
-
-import "time"
-
-func init() {
-	registerFunction("now", func(name string, v ...string) string {
-		return time.Now().Format(name)
-	})
-}
